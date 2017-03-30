b459e898b4b8ce8461fa80bdaf3223d2ebfb1157
Eros <blelarg@gmail.com>
2017-03-30 09:17:19 +0800

    resize image

--- NowNews/DetailViewController.swift
+++ NowNews/DetailViewController.swift
@@ -98,13 +98,38 @@ class DetailViewController: UIViewController, UIScrollViewDelegate {
                 
                 imageData.append(data!)
                 DispatchQueue.main.async {
-                    self.pic.image = UIImage(data: imageData)
+                    self.pic.image = self.resizeImage(image: UIImage(data: imageData)!, targetSize: CGSize(width: 375, height: 250))
                 }
             })
             dataTask.resume()
         }
     }
     
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
     func setColor(category: String) -> UIColor {
         switch (category) {
         case "focus":
